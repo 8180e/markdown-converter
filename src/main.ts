5605f2c9cc4838ffe9facad8e19f87600d6d2711
8180e <emirceylan31@hotmail.com>
2026-02-14 20:42:01 +0100

    Add converting to BBCode

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,52 @@ function parseMarkdownToSlides(md: string) {
   return slides.map((slide) => slide.replace(/^#\s+/gm, "").trim());
 }
 
+function markdownToBBCode(md: string) {
+  const tokens = marked.lexer(md);
+  let bbcode = "";
+
+  tokens.forEach((token) => {
+    switch (token.type) {
+      case "heading":
+        const size = 4 - token.depth;
+        bbcode += `[size=${size}]${token.text}[/size]\n\n`;
+        break;
+      case "paragraph":
+        bbcode += `${token.text}\n\n`;
+        break;
+      case "strong":
+        bbcode += `[b]${token.text}[/b]`;
+        break;
+      case "em":
+        bbcode += `[i]${token.text}[/i]`;
+        break;
+      case "list":
+        bbcode += "[list]\n";
+        token.items.forEach(({ text }: { text: string }) => {
+          bbcode += `[*]${text}\n`;
+        });
+        bbcode += "[/list]\n\n";
+        break;
+      case "code":
+        bbcode += `[code]${token.text}[/code]\n\n`;
+        break;
+      case "blockquote":
+        bbcode += `[quote]${token.text}[/quote]\n\n`;
+        break;
+      case "link":
+        bbcode += `[url=${token.href}]${token.text}[/url]`;
+        break;
+      case "image":
+        bbcode += `[img]${token.href}[/img]`;
+        break;
+      default:
+        bbcode += token.raw;
+    }
+  });
+
+  return bbcode;
+}
+
 const a = document.createElement("a");
 
 function download(filename: string, url: string) {
@@ -131,4 +177,9 @@ onButtonClick("convertToPPTXButton", () => {
   });
 
   pptx.writeFile({ fileName: "output.pptx" });
+});
+
+onButtonClick("convertToBBCodeButton", () => {
+  const bbcode = markdownToBBCode(markdownInput.value);
+  download("output.bbcode", createFileDownloadURL(bbcode, "text/plain"));
 });
